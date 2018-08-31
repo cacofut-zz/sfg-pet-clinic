1034486da9dee829a32669be37ba9d74c620ce4b
Cristiano Carvalho Amaral <cacofut@hotmail.com>
2018-08-31 18:49:30 -0300

    Seção 3, Aula 54

---
+++ src/main/java/br/com/diagnosticit/model/Pet.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.diagnosticit.model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author cristianoca
+ */
+public class Pet {
+    
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public Pet() {
+    }
+
+    public Pet(PetType petType, Owner owner, LocalDate birthDate) {
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+    
+    
+}
