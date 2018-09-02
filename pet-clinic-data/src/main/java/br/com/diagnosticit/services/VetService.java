1ca03ad62dbc5dac6d63499ae343371995ecf7d8
Cristiano <cacofut@hotmail.com>
2018-09-02 12:51:12 -0300

    Interface Services Adicionadas

---
+++ pet-clinic-data/src/main/java/br/com/diagnosticit/services/VetService.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.diagnosticit.services;
+
+import br.com.diagnosticit.model.Pet;
+import java.util.Set;
+
+/**
+ *
+ * @author cristiano
+ */
+public interface VetService {
+        
+    Pet findById(Long id);
+    
+    Pet save(Pet owner);
+    
+    Set<Pet> findAll();
+}
