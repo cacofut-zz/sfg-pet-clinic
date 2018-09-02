034fb9551e90ffc2162bd349d2400b4544b84067
Cristiano <cacofut@hotmail.com>
2018-09-02 15:21:07 -0300

    Serviço de mapa de dados

---
+++ pet-clinic-data/src/main/java/br/com/diagnosticit/services/map/PetServiceMap.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.diagnosticit.services.map;
+
+import br.com.diagnosticit.model.Pet;
+import br.com.diagnosticit.services.CrudService;
+import java.util.Set;
+
+/**
+ *
+ * @author cristiano
+ */
+public class PetServiceMap extends AbstractMapService<Pet, Long>  implements CrudService<Pet, Long>{
+    
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
