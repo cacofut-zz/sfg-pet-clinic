da3c2e3cf86767125315444ef5ecfb804911fc50
Cristiano <cacofut@hotmail.com>
2018-09-04 01:02:43 -0300

    página index e controlador criado. Closes #2

---
+++ pet-clinic-web/src/main/java/br/com/diagnosticit/controller/OwnerController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.diagnosticit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author cristiano
+ */
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+    
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
