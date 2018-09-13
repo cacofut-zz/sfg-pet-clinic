98cf79a6b77b9d7a9aece13672619511a633ea7f
Cristiano Carvalho Amaral <cacofut@hotmail.com>
2018-09-13 13:50:22 -0300

    Criado o método para número de auto-incremento ok. Closes #7

--- pet-clinic-data/src/main/java/br/com/diagnosticit/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/br/com/diagnosticit/services/map/AbstractMapService.java
@@ -5,18 +5,22 @@
  */
 package br.com.diagnosticit.services.map;
 
+import br.com.diagnosticit.model.BaseEntity;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 /**
  *
  * @author cristiano
  */
-public class AbstractMapService<T, ID>{
+public class AbstractMapService<T extends BaseEntity, ID extends Long>{
     
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
     
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -26,8 +30,15 @@ public class AbstractMapService<T, ID>{
         return map.get(id);
     }
     
-    T save( ID id, T object ){
-        map.put(id, object);
+    T save( T object ){
+        
+        if( object != null ){
+            if( object.getId() == null ){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }
+        
         return object;
     }
     
@@ -37,5 +48,17 @@ public class AbstractMapService<T, ID>{
     
     void delete( T object ){
         map.entrySet().removeIf( entry -> entry.getValue().equals(object));
+    }
+    
+    private Long getNextId(){
+        
+        Long nextId = null;
+        try{
+            nextId = Collections.max( map.keySet()) + 1;
+        }
+        catch( NoSuchElementException ex ){
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
