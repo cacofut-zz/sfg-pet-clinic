005d589a1a1bfefe87dfeef2fd9a7d50e15912ec
Cristiano <cacofut@hotmail.com>
2018-09-13 01:26:21 -0300

    Listar vets na index page concluído. Closes #6

--- pet-clinic-web/src/main/java/br/com/diagnosticit/controller/VetController.java
+++ pet-clinic-web/src/main/java/br/com/diagnosticit/controller/VetController.java
@@ -5,7 +5,10 @@
  */
 package br.com.diagnosticit.controller;
 
+import br.com.diagnosticit.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -14,9 +17,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class VetController {
-     
+    
+    private VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+           
     @RequestMapping( {"/vets", "/vets/index", "/vets/index.html"} )
-    public String index(){
+    public String index( Model model ){
+        model.addAttribute( "vets", vetService.findAll());
         return "vets/index";
     }
 }
